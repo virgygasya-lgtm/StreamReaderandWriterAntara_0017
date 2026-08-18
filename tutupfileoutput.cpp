27e168eab4577dfc80f45b9e43c6c88c488d8f30
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-18 10:26:39 +0700

    tutupfileoutput.cpp

--- tutupfileoutput.cpp
+++ tutupfileoutput.cpp
@@ -51,3 +51,32 @@ int main()
 
     return 0;
 }
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string baris;
+
+    ofstream outfile;
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    while (true)
+    {
+        cout << "- ";
+        getline(cin, baris);
+
+        if (baris == "q") break;
+
+        outfile << baris << endl;
+    }
+
+    outfile.close();
+
+    return 0;
+}
