7d9ba3918041b8cdfcb082d01d1585f30161674d
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-18 10:22:19 +0700

    menambahkandefaultcatch.cpp

--- menambahkandefaultcatch.cpp
+++ menambahkandefaultcatch.cpp
@@ -57,3 +57,31 @@ int main()
 
     return 0;
 }
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //membuat isi dari try untuk pengujian error
+    try
+    {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+        throw 0.5; //melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan ditampilkan" << endl;
+    }
+
+    //kondisi jika throw yang bertipe data integer
+    catch (int a)
+    {
+        cout << "Pengecualian akan di eksekusi" << endl;
+    }
+
+    //kondisi jika throw bertipe data selain integer
+    catch (...)
+    {
+        cout << "Default pengecualian dieksekusi" << endl;
+    }
+
+    return 0;
+} 
