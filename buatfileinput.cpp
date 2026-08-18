df14cd4c2c170fa96177fa9e15cd93b9a3f98d8d
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-18 10:27:21 +0700

    buatfileinput.cpp

--- buatfileinput.cpp
+++ buatfileinput.cpp
@@ -80,3 +80,35 @@ int main()
 
     return 0;
 }
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string baris;
+
+    ofstream outfile;
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    while (true)
+    {
+        cout << "- ";
+        getline(cin, baris);
+
+        if (baris == "q") break;
+
+        outfile << baris << endl;
+    }
+
+    outfile.close();
+
+    ifstream infile;
+    infile.open("contohfile.txt");
+
+    return 0;
+}
