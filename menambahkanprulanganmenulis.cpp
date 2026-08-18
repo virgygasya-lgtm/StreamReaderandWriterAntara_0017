d3bb011c6ca72727b4425773344e3172ca9c55ce
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-18 10:14:25 +0700

    menambahkanprulanganmenulis.cpp

--- menambahkanprulanganmenulis.cpp
+++ menambahkanprulanganmenulis.cpp
@@ -46,3 +46,41 @@ int main()
 
     cout << "=> Menulis file, 'q' untuk keluar" << endl;
 } 
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    //deklarasi variabel baris dan NamaFile
+    string baris, NamaFile;
+
+    //membuat input untuk menangkap nama file
+    cout << "Masukkan nama file : ";
+    cin >> NamaFile;
+
+    //membuat file dalam mode menulis
+    ofstream outfile;
+
+    //menunjuk ke sebuah nama file dengan ekstensi txt
+    outfile.open("C:\\Users\\LENOVO\\Documents\\contoh\\" + NamaFile + ".txt", ios::out);
+
+    cout << "=> Menulis file, 'q' untuk keluar" << endl;
+
+    //looping untuk menulis isi file
+    while (true)
+    {
+        cout << " - ";
+
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+
+        //kondisi jika baris hanya berisi huruf q
+        if (baris == "q") break;
+
+        //menulis dan memasukkan nilai dari baris ke dalam file
+        outfile << baris << endl;
+    }
+}
