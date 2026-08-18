9f2f706b04e55de1ff8829bf0c43e99548f3d9ff
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-18 10:27:54 +0700

    bacaisifile.cpp

--- bacaisifile.cpp
+++ bacaisifile.cpp
@@ -112,3 +112,47 @@ int main()
 
     return 0;
 }
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string baris;
+
+    ofstream outfile;
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    while (true)
+    {
+        cout << "- ";
+        getline(cin, baris);
+
+        if (baris == "q") break;
+
+        outfile << baris << endl;
+    }
+
+    outfile.close();
+
+    ifstream infile;
+    infile.open("contohfile.txt");
+
+    cout << endl << ">=Membuka dan membaca file " << endl;
+
+    if (infile.is_open())
+    {
+        while (getline(infile, baris))
+        {
+            cout << baris << endl;
+        }
+
+        infile.close();
+    }
+
+    return 0;
+}
