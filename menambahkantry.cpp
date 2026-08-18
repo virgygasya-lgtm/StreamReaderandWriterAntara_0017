5eead3dd25a951e0a8cc48cece68cda342efe083
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-18 10:20:09 +0700

    menambahkantry.cpp

---
+++ menambahkantry.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //membuat isi dari try untuk pengujian error
+    try
+    {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+    }
+
+    return 0;
+}
