b64216ff706f07418d1b443a61470fb77a091b78
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-18 10:12:34 +0700

    menambahkaninputnamafile.cpp

---
+++ menambahkaninputnamafile.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    //deklarasi variabel baris dan NamaFile
+    string baris, NamaFile;
+}
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    //deklarasi variabel baris dan NamaFile
+    string baris, NamaFile;
+
+    //membuat input untuk menangkap nama file
+    cout << "Masukkan nama file : ";
+    cin >> NamaFile;
+}
