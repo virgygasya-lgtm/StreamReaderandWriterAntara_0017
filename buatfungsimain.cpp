2aa38ff0dad7d0d132fe12898063c158565ca3a2
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-18 10:07:00 +0700

    buatfungsimain.cpp

---
+++ buatfungsimain.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <exception>
+#include <array>
+using namespace std; 
+
+#include <iostream>
+#include <exception>
+#include <array>
+using namespace std;
+
+int main()
+{
+    cout << "Awal program" << endl; //penanda 1
+
+    return 0;
+}
--- importlibrary.cpp
+++
@@ -1,4 +0,0 @@
-#include <iostream>
-#include <exception>
-#include <array>
-using namespace std; 
