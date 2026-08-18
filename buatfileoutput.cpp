2200aabe5a5b8393fcaf95f3d4e21ddbb32ccbdc
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-18 10:25:11 +0700

    buatfileoutput.cpp

---
+++ buatfileoutput.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string baris;
+
+    return 0;
+}
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string baris;
+
+    ofstream outfile;
+    outfile.open("contohfile.txt");
+
+    return 0;
+}
